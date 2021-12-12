50f55c88b8cffd505df5dc542e8e3c74e77237f2
JamesGolian <golian.james0420@gmail.com>
2021-12-12 16:18:37 +0100

    deploy candy machine and update ui with mint information

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import CandyMachine from './candy-machine';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -75,6 +76,7 @@ const App = () => {
           <p className="sub-text">NFT drop machine with fair mint</p>
           {!walletAddress && renderNotConnectedContainer()}
         </div>
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
